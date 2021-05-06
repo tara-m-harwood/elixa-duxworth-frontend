9b8f30e54aa281ff3459ce858233a7c8bf9550d6
Tara Harwood <tara.m.harwood@gmail.com>
2021-05-06 12:17:07 -0700

    testing sidebar component

--- src/ActionProvider.js
+++ src/ActionProvider.js
@@ -9,14 +9,13 @@ class ActionProvider {
     elixa(message) {
     
         const baseURL = "https://api.pandorabots.com/talk/unf6963c69/elixa?user_key=bc3d30376b455afd667908e211239116&input="
+        const options = { method: 'POST'}
 
-        fetch(`${baseURL}${message}`, {
-                    method: 'POST'
-                }
-            )
+        fetch(`${baseURL}${message}`, options)
             .then(data => data.json())
             .then(data => {
                 const responses = data.responses
+                
                 responses.forEach(response => {
                     const elixaReply =this.createChatBotMessage(response);
                     this.updateChatbotState(elixaReply)
---
+++ src/components/ElixaSidebar/ElixaSidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import elixaIcon from "./elixa_avatar.png"
+
+const ElixaSidebar = () => {
+  return (
+        <section id="left-sidebar">
+          <img id="elixa-sidebar-icon" src={elixaIcon} alt="friendly woman" />
+        </section>
+  );
+};
+
+export default ElixaSidebar;
