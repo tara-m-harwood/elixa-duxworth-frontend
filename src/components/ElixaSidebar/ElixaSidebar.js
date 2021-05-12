86ad09d071d20de89fa4fcfc1f710dd6216b6bf1
Tara Harwood <tara.m.harwood@gmail.com>
2021-05-11 19:12:46 -0700

    built retrieve form

--- src/components/ElixaSidebar/ElixaSidebar.js
+++ src/components/ElixaSidebar/ElixaSidebar.js
@@ -7,27 +7,47 @@ export default class ElixaSidebar extends Component {
         super(props);
 
         this.state = {
-            showModal: false,
-            modalInputName: "",
-            modalInputPhone: "",
+            showModalSave: false,
+            showModalRetrieve: false,            
+            modalSaveInputName: "",
+            modalSaveInputPhone: "",
+            modalRetrieveInputName: "",
+            modalRetrieveInputPhone: "",
         };
     }
 
-    getModalClass = () => {
-        if(this.state.showModal){
+    getModalSaveClass = () => {
+        if(this.state.showModalSave){
             return "show-modal"
         } else {
             return "hide-modal"
         } 
     }
 
-    modalOpen(e) {
-        console.log("open sesame")
-        this.setState({ showModal: true });
+    getModalRetrieveClass = () => {
+        if(this.state.showModalRetrieve){
+            return "show-modal"
+        } else {
+            return "hide-modal"
+        } 
     }
 
-    modalClose(e) {
-        this.setState({ showModal: false });
+    modalSaveOpen() {
+        console.log("open sesame save")
+        this.setState({ showModalSave: true });
+    }
+
+    modalSaveRetrieve() {
+        console.log("open sesame retrieve")
+        this.setState({ showModalRetrieve: true });
+    }
+
+    modalSaveClose() {
+        this.setState({ showModalSave: false });
+    }
+
+    modalRetrieveClose() {
+        this.setState({ showModalRetrieve: false });
     }
 
     handleChange(e) {
@@ -41,13 +61,13 @@ export default class ElixaSidebar extends Component {
     }
 
     handleSubmit(e) {
-        this.setState({ name: this.state.modalInputName });
-        this.setState({ name: this.state.modalInputPhone });
-        console.log(this.state.modalInputName)
-        console.log(this.state.modalInputPhone)
-        testIt(this.state.modalInputName, this.state.modalInputPhone)
-        pbSaveClient(this.state.modalInputName, this.state.modalInputPhone)
-        this.modalClose();
+        this.setState({ name: this.state.modalSaveInputName });
+        this.setState({ name: this.state.modalSaveInputPhone });
+        console.log(this.state.modalSaveInputName)
+        console.log(this.state.modalSaveInputPhone)
+        testIt(this.state.modalSaveInputName, this.state.modalSaveInputPhone)
+        pbSaveClient(this.state.modalSaveInputName, this.state.modalSaveInputPhone)
+        this.modalSaveClose();
     }
 
     testFunc(e){
@@ -65,20 +85,21 @@ export default class ElixaSidebar extends Component {
                 </div>
 
                 <div id="left-sidebar-button-container">
-                    <button id="left-sidebar-button-save" onClick={e => this.modalOpen(e)}>
+                    <button id="left-sidebar-button-save" onClick={e => this.modalSaveOpen()}>
                         save session
                     </button>
                     <button id="left-sidebar-button-retrieve">retrieve session</button>
                 </div>
 
-                <div id="#modal-content-container" className={this.getModalClass()}>
+                {/* SAVE MODAL */}
+                <div id="#modal-save-container" className={this.getModalSaveClass()}>
                     <form id="save-modal-form">
-                        <button id="closeModalSave" onClick={e => this.modalClose(e)} type="button">X</button>
+                        <button id="closeModalSave" onClick={e => this.modalSaveClose()} type="button">X</button>
                         <label className="form-label">
                             Your first name:
                             <input
                                 type="text"
-                                name="modalInputName"
+                                name="modalSaveInputName"
                                 value={this.state.modalInputName}
                                 onChange={e => this.handleChange(e)}
                                 className="form-control"
@@ -88,15 +109,48 @@ export default class ElixaSidebar extends Component {
                             Your phone number
                             <input
                                 type="text"
-                                name="modalInputPhone"
+                                name="modalSaveInputPhone"
                                 value={this.state.modalInputPhone}
                                 onChange={e => this.handleChange(e)}
                                 className="form-control"
                             />
                         </label>
                         <div id="save-session-container">
-                            <button id="save-session" className="form-control" onClick={e => this.handleSubmit(e)} type="button">
+                            <button id="save-session" className="form-control" 
+                                    onClick={e => this.handleSubmit(e)} type="button">
                                 Save
+                            </button>
+                        </div>
+                    </form>
+                </div>
+
+                {/* RETRIEVE MODAL */}
+                <div id="#modal-retrieve-container" className={this.getModalRetrieveClass()}>
+                    <form id="retrieve-modal-form">
+                        <button id="closeModalRetrieve" onClick={e => this.modalRetrieveClose(e)} type="button">X</button>
+                        <label className="form-label">
+                            Your first name:
+                            <input
+                                type="text"
+                                name="modalRetrieveInputName"
+                                value={this.state.modalRetrieveInputName}
+                                onChange={e => this.handleChange(e)}
+                                className="form-control"
+                            />
+                        </label>
+                        <label className="form-label">
+                            Your phone number
+                            <input
+                                type="text"
+                                name="modalInputRetrievePhone"
+                                value={this.state.modalRetrieveInputPhone}
+                                onChange={e => this.handleChange(e)}
+                                className="form-control"
+                            />
+                        </label>
+                        <div id="retrieve-session-container">
+                            <button id="retrieve-session" className="form-control" onClick={e => this.handleSubmit(e)} type="button">
+                                Retrieve
                             </button>
                         </div>
                     </form>
